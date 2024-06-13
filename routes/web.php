df9d33539068f34230594023420096dc509d1579
k4ik <kaikleal1234@gmail.com>
2024-06-13 13:31:27 -0300

    feat: routes

---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
